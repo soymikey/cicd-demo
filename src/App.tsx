4148f3d5e811bd8eb67453cf51e5f4e5b436c8cd
Michael ChengMQ <michael.chengmq@finpoints.com>
2023-07-30 13:36:35 +0800

    feat: add feature-v.6.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ const App: FC<Props> = ({ value }) => {
         <p>feature-v.4.0.0</p>
         <p>feature-v.4.0.1</p>
         <p>feature-v.5.0.0</p>
+        <p>feature-v.6.0.0</p>
         <p>
           <button role="button" type="button" onClick={() => setCount((v) => v + 1)}>
             count is: {count}
